8ad9e022a19b451c37671f799e5868b3b3812fda
GhosTHaise <ghostrex2@gmail.com>
2025-05-04 22:39:29 +0300

    feat : init mcp server

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mcp = FastMCP("docs")
+
+USER_AGENT = "docs-app/1.0"
+SERPER_URL = ""
+
 def main():
     print("Hello from mcp-document-server!")
 
