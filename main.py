d14f2519da104dcd5a9d6051ae5120f5313f84e1
GhosTHaise <ghostrex2@gmail.com>
2025-05-04 22:42:10 +0300

    feat : func and docs url

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ mcp = FastMCP("docs")
 USER_AGENT = "docs-app/1.0"
 SERPER_URL = ""
 
+docs_urls = {
+    "langchain" : "python.langchain.com/docs",
+    "llama-index" : "docs.llamaindex.ai/en/stable",
+    "openai" : "platform.openai.com/docs"
+}
+
+def search_web():
+    ...
+
+def fetch_url():
+    ...
+
+@mcp.tool()
+def get_docs():
+    ...
+
 def main():
     print("Hello from mcp-document-server!")
 
